ed64ac87824dd51bb5f347d1a6ff169026f14391
KiriZhu <kiribaty88@gmail.com>
2020-09-11 12:01:07 +0200

    parsing the matches string by spliting it on each new row and then mapping each row to an array that is split at the commas

--- build/index.js
+++ build/index.js
@@ -1,2 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+//encoding tells readfilesync whta type of content we expect to find inside the csv file. utf8 text content (string) otherwise we would get a buffer
+//matches is now a big string that contains all diff matches
+var matches = fs_1.default
+    .readFileSync('football.csv', { encoding: 'utf-8' })
+    .split('\n')
+    .map(function (row) {
+    return row.split(',');
+});
+// first we split on row
+//then we map the rows to and array of strings that are split on the comma.
+console.log(matches);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 import fs from 'fs';
+//encoding tells readfilesync whta type of content we expect to find inside the csv file. utf8 text content (string) otherwise we would get a buffer
+
+//matches is now a big string that contains all diff matches
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+// first we split on row
+//then we map the rows to and array of strings that are split on the comma.
+
+console.log(matches);
