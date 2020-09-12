1a4bf06ab5a67840a14601c186f349880cdb07ab
KiriZhu <kiribaty88@gmail.com>
2020-09-12 22:45:22 +0200

    refactored csv reader to be reusable and matchreader to extend it using generics in  order to avoid usin any type

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -5,13 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CsvFileReader = void 0;
 var fs_1 = __importDefault(require("fs"));
-//encoding tells readfilesync whta type of content we expect to find inside the csv file. utf8 text content (string) otherwise we would get a buffer
-var utils_1 = require("./utils");
+//encoding utf8 = expect text content
+//Generic = <T>
 var CsvFileReader = /** @class */ (function () {
     function CsvFileReader(filename) {
         this.filename = filename;
-        // first we split on row
-        //then we map the rows to and array of strings that are split on the comma.
         this.data = [];
     }
     CsvFileReader.prototype.read = function () {
@@ -21,17 +19,7 @@ var CsvFileReader = /** @class */ (function () {
             .map(function (row) {
             return row.split(',');
         })
-            .map(function (row) {
-            return [
-                utils_1.dateStringToDate(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                row[5],
-                row[6],
-            ];
-        });
+            .map(this.mapRow);
     };
     return CsvFileReader;
 }());
--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,18 +1,13 @@
 import { CsvFileReader } from './CsvFileReader';
-import { dateStringToDate } from "./utils";
-import { MatchResult } from "./MatchResult";
-export class MatchReader extends CsvFileReader {
-  mapRow(row: string): MatchData {
+import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResult';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export class MatchReader extends CsvFileReader<MatchData> {
+  mapRow(row: string[]): MatchData {
     return [
-      return [
       dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult, // Type assertion
-      row[6],
-    ];(row[0]),
       row[1],
       row[2],
       parseInt(row[3]),
