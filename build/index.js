8d88dc8b0e980868362b8ffb704ef67357c69458
KiriZhu <kiribaty88@gmail.com>
2020-09-15 23:32:57 +0200

    removed unused imports and comments

--- build/index.js
+++ build/index.js
@@ -2,12 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var Summary_1 = require("./Summary");
-//create an object that satisfies the DataReader interface
-// const csvFileReader = new CsvFileReader('football.csv');
-//create an instance of MatchReader and pass something  satisfying the DataReader interface
-// const matchReader = new MatchReader(csvFileReader);
 var matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
-matchReader.load();
-// const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 var summary = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
