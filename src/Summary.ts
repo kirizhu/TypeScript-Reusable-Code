e8c9465e740e572945e97babab13f1ad4db14d1b
KiriZhu <kiribaty88@gmail.com>
2020-09-15 22:58:52 +0200

    created ConsoleReport class that implements outputTarget and prints the output passed to it

--- src/Summary.ts
+++ src/Summary.ts
@@ -9,6 +9,11 @@ export interface OutputTarget {
 
 export class Summary {
   constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 }
 //Example of how we would instantiate
 //new Summary(new WinsAnalysis(), new ConsoleReport());
