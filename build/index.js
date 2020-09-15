0675eb4bf58ef77132f274e62173b48b223b1a20
KiriZhu <kiribaty88@gmail.com>
2020-09-15 23:30:13 +0200

    more refactoring with static methods so that we can call it without instantiatin its class

--- build/index.js
+++ build/index.js
@@ -1,14 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var CsvFileReader_1 = require("./CsvFileReader");
-var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
-var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 var Summary_1 = require("./Summary");
 //create an object that satisfies the DataReader interface
-var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+// const csvFileReader = new CsvFileReader('football.csv');
 //create an instance of MatchReader and pass something  satisfying the DataReader interface
-var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+var matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
 matchReader.load();
-var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+// const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+var summary = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
 summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 //create an object that satisfies the DataReader interface
-const csvFileReader = new CsvFileReader('football.csv');
+// const csvFileReader = new CsvFileReader('football.csv');
 
 //create an instance of MatchReader and pass something  satisfying the DataReader interface
-const matchReader = new MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
+// const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 summary.buildAndPrintReport(matchReader.matches);
